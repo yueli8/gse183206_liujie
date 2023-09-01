2f2be7a808251ed90e15a894f826d73ecf8ed415
yueli8 <40526984+yueli8@users.noreply.github.com>
2023-09-01 15:29:18 +0800

    Update gse183206_liujie.R

--- gse183206_liujie.R
+++ gse183206_liujie.R
@@ -3,5 +3,5 @@ library(hdf5r)
 library(rhdf5)
 
 setwd("~/gse183206_liujie")
-seurat_data <- Read10X_h5("GSE183206_aggr_filtered_counts_matrix .h5")#complete download the file.File has been trncated.
+seurat_data <- Read10X_h5("GSE183206_aggr_filtered_counts_matrix .h5")#complete download the file.File has been truncated.
 
